7c8b6f1aac1b9f5860712f989f7c02dfe24f0a41
GauravRajpt <joygrajput@gmail.com>
2023-09-06 22:10:46 +0530

    from data save in user array

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+const exp = require('constants');
 const express= require('express')
 const path = require('path');
 const app= express();
 
+
+const user=[];
+
+const static= express.static(__dirname+"/public");
+app.use(express.urlencoded({extended:true}))
+app.use(static); //using middleware
+app.set("view engine", "ejs")
 app.get("/",(req,res)=>{
 
-    const pathlocation= path.join(__dirname,"/index.html")
-    res.sendFile(pathlocation)
+    res.render("index.ejs",{
+        name:"gaurav"
+    });
+})
+
+app.get('/contact',(req,res)=>{
+    res.send(user)
+})
+
+app.post("/contact",(req,res)=>{
+    
+    user.push({
+        username:req.body.name,
+        email:req.body.email,
+    
+    })
+    res.render("success")
 })
 
 app.listen(5000,()=>{
