1924eafeab55565fb1fae579d985526091eec4e2
GauravRajpt <joygrajput@gmail.com>
2023-09-07 20:29:31 +0530

    create collections or send data in db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const exp = require('constants');
 const express= require('express');
-const { default: mongoose } = require('mongoose');
+const { default: mongoose, Schema, Collection } = require('mongoose');
 const path = require('path');
 const app= express();
 
@@ -12,29 +12,29 @@ mongoose.connect('mongodb://127.0.0.1:27017',{
 }).catch(e=>console.log(e))
 
 
+const MessageSchema= new Schema({
+    name:String,
+    email:String,
+},{collection:"Message"})
+
+const Message= mongoose.model("Message",MessageSchema)
 const static= express.static(__dirname+"/public");
 app.use(express.urlencoded({extended:true}))
 app.use(static); //using middleware
 app.set("view engine", "ejs")
 app.get("/",(req,res)=>{
-
+    
     res.render("index.ejs",{
         name:"gaurav"
     });
 })
 
-app.get('/contact',(req,res)=>{
-    res.send(user)
-})
+
 
 app.post("/contact",(req,res)=>{
-    
-    user.push({
-        username:req.body.name,
-        email:req.body.email,
-    
-    })
-    res.render("success")
+   
+    Message.create({name:req.body.name,email:req.body.email})
+    res.send(req.body.name)
 })
 
 app.listen(5000,()=>{
