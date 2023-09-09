0ad36a42c6ab2f7efe7bd0d67d198914660d3559
GauravRajpt <joygrajput@gmail.com>
2023-09-09 23:15:55 +0530

    jwt

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { default: mongoose, Schema, Collection } = require('mongoose');
 const path = require('path');
 const app= express();
 const cookieparser= require("cookie-parser");
+const jwt= require("jsonwebtoken")
 
 
 mongoose.connect('mongodb://127.0.0.1:27017',{
@@ -16,34 +17,43 @@ mongoose.connect('mongodb://127.0.0.1:27017',{
 const MessageSchema= new Schema({
     name:String,
     email:String,
-},{collection:"Message"})
+},{collection:"users"})
 
-const isAuthenticate=(req,res,next)=>{
+
+
+const isAuthenticate=async (req,res,next)=>{
     const {token}= req.cookies;
-
+   
+  
+    
     if(token){
+         const decoded= jwt.verify(token,"kdjfkdjf");
+       req.user= await User.findById(decoded._id);
+       console.log(req.user);
         next()
     }
     else{
-        res.render("login")
+        res.render("index",)
     }
 
    
 }
 
-const Message= mongoose.model("Message",MessageSchema)
+const User= mongoose.model("users",MessageSchema)
 const static= express.static(__dirname+"/public");
 app.use(express.urlencoded({extended:true}))
 app.use(cookieparser());
 app.use(static); //using middleware
 app.set("view engine", "ejs")
 app.get("/",isAuthenticate,(req,res)=>{
-    res.render("logout");
+    res.render("logout", {name:req.user.name});
 
    
 })
 
 app.get("/logout",(req,res)=>{
+
+   
     res.cookie("token",null,{
         httpOnly:true,
         expires:new Date(Date.now()),
@@ -51,8 +61,23 @@ app.get("/logout",(req,res)=>{
     res.redirect("/")
 })
 
-app.post("/login",(req,res)=>{
-   res.cookie("token","123",{
+app.post("/login",async (req,res)=>{
+
+   
+   
+    
+    const {name, email}= req.body;
+    let user= await User.findOne({ email });
+    if(!user){
+        return console.log("user does not exit");
+        }
+         
+ user= await User.create({
+        name,
+        email
+    })
+    const jwtoken= jwt.sign({_id:user._id,},"kdjfkdjf");
+   res.cookie("token",jwtoken,{
     httpOnly:true,
     expires:new Date(Date.now()+60*1000)
    })
